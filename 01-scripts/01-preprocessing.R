77bcb1ebacd2c31d2389a196e93370e8993ecad1
Juan M. Barrios <j.m.barrios@gmail.com>
2025-12-19 09:07:28 -0600

    feat: se completó el flujo de creación de los datos procesados
    Ya se tiene la creación total del conjunto de datos por cuenca.
    
    Además se corrigió un problema en la función `add_stat_raster`.

--- 01-scripts/01-preprocessing.R
+++ 01-scripts/01-preprocessing.R
@@ -1,6 +1,8 @@
 library(tidyverse)
 library(janitor)
 library(sf)
+library(stars)
+library(starsExtra)
 
 # Util functions ----
 
@@ -112,7 +114,7 @@ add_stat_raster <- function(data,
   
   # Ensure CRS compatibility
   if (!sf::st_crs(data) == sf::st_crs(raster_covar)) {
-    raster_covar <- sf::st_transform(raster_covar, sf::st_crs(data))
+    raster_covar <- stars::st_warp(raster_covar, crs = sf::st_crs(data))
   }
   
   # Convert single function to named list
@@ -126,98 +128,70 @@ add_stat_raster <- function(data,
     names(funs) <- paste0("fun", seq_along(funs))
   }
   
-  # Get dimension names and handle bands
+  # Select bands if specified
   dims <- names(dim(raster_covar))
   band_dim <- dims[!(dims %in% c("x", "y"))]
   
-  # Select bands if specified
   if (!is.null(bands) && length(band_dim) > 0) {
     raster_covar <- raster_covar %>%
       dplyr::slice(!!rlang::sym(band_dim[1]), bands)
-  }
-  
-  # Get band names/indices
-  if (length(band_dim) > 0) {
-    band_values <- stars::st_get_dimension_values(raster_covar, band_dim[1])
-    if (is.null(band_values)) {
-      band_values <- seq_len(dim(raster_covar)[band_dim[1]])
-    }
-  } else {
-    band_values <- 1
-    band_dim <- NULL
   }
   
   # Add row identifier
   data <- data %>%
     dplyr::mutate(.row_id = dplyr::row_number())
   
-  # Extract raster values for each feature
-  extracted_values <- stars::st_extract(raster_covar, data)
+  # Use aggregate to extract values for each polygon
+  # This is more reliable than st_extract for getting actual pixel values
+  result_list <- list()
   
-  # Convert to tibble for easier manipulation
-  extracted_df <- extracted_values %>%
-    tibble::as_tibble() %>%
-    dplyr::mutate(.row_id = dplyr::row_number())
-  
-  # Get the name of the raster attribute
-  raster_attr <- names(extracted_df)[1]
-  
-  # Process based on whether we have bands or not
-  if (!is.null(band_dim) && length(band_values) > 1) {
-    # Multiple bands case
-    result_stats <- extracted_df %>%
-      dplyr::select(.row_id, dplyr::all_of(raster_attr)) %>%
-      tidyr::unnest(cols = dplyr::all_of(raster_attr)) %>%
-      dplyr::group_by(.row_id, !!rlang::sym(band_dim[1])) %>%
-      dplyr::summarise(
-        dplyr::across(
-          .cols = 1,  # The raster values column
-          .fns = funs,
-          .names = "{.fn}",
-          na.rm = na.rm
-        ),
-        .groups = "drop"
-      ) %>%
-      tidyr::pivot_longer(
-        cols = -c(.row_id, !!rlang::sym(band_dim[1])),
-        names_to = ".stat",
-        values_to = ".value"
-      ) %>%
-      dplyr::mutate(
-        .col_name = paste(column_prefix, !!rlang::sym(band_dim[1]), .stat, sep = "_")
-      ) %>%
-      dplyr::select(.row_id, .col_name, .value) %>%
-      tidyr::pivot_wider(
-        names_from = .col_name,
-        values_from = .value
-      )
-  } else {
-    # Single band case
-    result_stats <- extracted_df %>%
-      dplyr::select(.row_id, dplyr::all_of(raster_attr)) %>%
-      tidyr::unnest(cols = dplyr::all_of(raster_attr)) %>%
-      dplyr::group_by(.row_id) %>%
-      dplyr::summarise(
-        dplyr::across(
-          .cols = 1,  # The raster values column
-          .fns = funs,
-          .names = paste0(column_prefix, "_{.fn}"),
-          na.rm = na.rm
-        ),
-        .groups = "drop"
-      )
+  for (fn_name in names(funs)) {
+    fn <- funs[[fn_name]]
+    
+    # Create wrapper function that handles na.rm
+    wrapper_fn <- function(x) {
+      if (na.rm) {
+        x <- x[!is.na(x)]
+      }
+      if (length(x) == 0) return(NA_real_)
+      fn(x)
+    }
+    
+    # Aggregate raster by polygons
+    agg_result <- aggregate(
+      raster_covar,
+      by = data,
+      FUN = wrapper_fn
+    )
+    
+    # Extract values
+    col_name <- paste0(column_prefix, "_", fn_name)
+    result_list[[col_name]] <- agg_result[[1]]
   }
   
-  # Join results back to original data
-  result <- data %>%
-    dplyr::left_join(result_stats, by = ".row_id") %>%
-    dplyr::select(-.row_id)
+  # Combine all statistics
+  result_stats <- tibble::as_tibble(result_list) %>%
+    janitor::clean_names()
+  
+  # Bind to original data
+  result <- dplyr::bind_cols(
+    data %>% dplyr::select(-.row_id),
+    result_stats
+  )
   
   return(result)
 }
 
 
 # Dataset creation ----
+OUTPUT_DIR <- '02-processed_data'
+shp_output <- fs::path_join(c(OUTPUT_DIR, "aoi_shp"))
+tiff_output <- fs::path_join(c(OUTPUT_DIR, "aoi_tiff"))
+
+fs::dir_create(shp_output)
+fs::dir_create(tiff_output)
+
+
 nanocuecas_data <- sf::read_sf('00-raw_data/nanocuencas/SHT_BAconsensuadov2_uw.shp')
 
 geologia_data <- sf::st_read('00-raw_data/geologia/r250k_ccl_dissolveTipo.shp')
@@ -226,7 +200,7 @@ usvVII_data <- sf::read_sf('00-raw_data/uso_suelo_vegetacion/usv250s7cw.shp')
 
 st_crs(suelos_data) <- geologia_data %>% st_crs()
 
-aoi_bbox <- nanocuecas_data %>% st_bbox() %>% st_as_sfc()
+aoi_bbox <- nanocuecas_data %>% st_bbox() %>% st_as_sfc() %>% st_buffer(3000) %>% st_bbox() %>% st_as_sfc()
 
 geologia_data_poly <- st_cast(geologia_data, "POLYGON", 
                               group_or_split = FALSE) # Convert mutipolygon to polygon
@@ -248,25 +222,85 @@ suelos_data_aoi <- suelos_data %>%
 usvVII_data_aoi <- usvVII_data %>% 
   st_filter(aoi_bbox)
 
+# Save shp
+geologia_data_aoi %>% 
+  st_write(fs::path_join(c(shp_output, 'geologia.shp')))
+
+suelos_data_aoi %>% 
+  st_write(fs::path_join(c(shp_output, 'suelos.shp')))
+
+usvVII_data_aoi %>% 
+  st_write(fs::path_join(c(shp_output, 'usvVII.shp')))
+
+
+
+# Add covar data raster
+
+world_dem <- read_stars('00-raw_data/DEM/wc2.1_30s_elev.tif')
+dem_crs <- world_dem %>% st_crs()
+
+# Crop to AOI
+aoi_bbox_t <- aoi_bbox %>% 
+  st_transform(dem_crs)
+aoi_dem <- world_dem %>% 
+  st_crop(aoi_bbox_t) %>% 
+  st_as_stars()
+
+# Transform to projected data and slope calculation
+aoi_bbox_grid <- aoi_bbox %>% 
+  st_as_stars()
+aoi_dem_proj <- aoi_dem %>% 
+  st_warp(aoi_bbox_grid) 
+aoi_dem_slope <- aoi_dem_proj %>% 
+  starsExtra::slope()
+
+# Save raster data
+aoi_dem %>% 
+  write_stars(fs::path_join(c(tiff_output, 'dem_aoi.tiff')))
+
+aoi_dem_slope %>% 
+  write_stars(fs::path_join(c(tiff_output, 'dem_slope_aoi.tiff')))
+
+# Create dataset ----
+
 # Select columns of interest
-geologia_data_aoi <- geologia_data_aoi %>% 
+geologia_data_aoi <- geologia_data_aoi %>%
   select(TIPO)
 
-suelos_data_aoi <- suelos_data_aoi %>% 
+suelos_data_aoi <- suelos_data_aoi %>%
   select(CLAVE_IMP)
 
-usvVII_data_aoi <- usvVII_data_aoi %>% 
+usvVII_data_aoi <- usvVII_data_aoi %>%
   select(codigo)
 
 nanocuecas_data <- nanocuecas_data %>%
   select(ESTADO_SHT, CLAVE_SHT)
 
-# 
+# Add covar data - shapefile
 dataset <- nanocuecas_data %>% 
-  add_area_covar(geologia_data_aoi, "TIPO", column_prefix = "geologia")
+  add_area_covar(geologia_data_aoi, "TIPO", unit = 'ha', column_prefix = "glg")
 
 dataset <- dataset %>% 
-  add_area_covar(suelos_data_aoi, "CLAVE_IMP", column_prefix = "suelos")
+  add_area_covar(suelos_data_aoi, "CLAVE_IMP", unit = 'ha', column_prefix = "suelos")
 
 dataset <- dataset %>% 
-  add_area_covar(usvVII_data_aoi, "codigo", column_prefix = "usv")
+  add_area_covar(usvVII_data_aoi, "codigo", unit = 'ha', column_prefix = "usv")
+
+# Add covar data - raster 
+fn_list <- list(
+  "min" = min,
+  "max" = max,
+  "median" = median,
+  "mean" = mean,
+  "sd" = sd
+)
+
+dataset <- dataset %>% 
+  add_stat_raster(aoi_dem_slope, funs = fn_list, column_prefix = "slope", na.rm = TRUE)
+
+# Save dataset shp
+dataset %>% st_write(fs::path_join(c(shp_output, "dataset.gpkg")))
+
+dataset %>% 
+  st_drop_geometry() %>% 
+  write_csv(fs::path_join(c(OUTPUT_DIR, "dataset.csv")))
